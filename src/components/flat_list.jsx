66b8523f9cbdfb66c488c76963bbb105e7076afc
Marine Sourin <marinesourin@live.fr>
2019-09-19 17:11:19 +0200

    flat_list.jsx

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import Flat from "./flat";
+import React from 'react';
+import Flat from './flat';
 
 const FlatList = (props) => {
-  const renderList = () => {
-    this.props.flats.map((flat, index) => {
-      return {
-        console.log(flat);
-        console.log(index);
-      };
+  const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.lat}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectFlat={props.selectFlat}
+        />
+      );
     });
   };
 
-  return {
-
-  }
+  return (
+    <div className="flat-list">
+      {renderList()}
+    </div>
+  );
 };
 
-export default FlatList
+export default FlatList;
